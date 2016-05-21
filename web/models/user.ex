2c62e4a129933b3af97595c822797983df15819e
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-05-21 11:24:16 +0200

    add user model

---
+++ web/models/user.ex
@@ -0,0 +1,42 @@
+defmodule Eliscore.User do
+  use Eliscore.Web, :model
+
+  schema "users" do
+    field :login, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :encrypted_password, :string
+
+    timestamps
+  end
+
+  @derive {Poison.Encoder, only: [:id, :login, :email]}
+
+  @required_fields ~w(login email password)
+  @optional_fields ~w(encrypted_password)
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, message: "Password does not match")
+    |> unique_constraint(:email, message: "Email already taken")
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(current_changeset) do
+    case current_changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(current_changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        current_changeset
+    end
+  end
+end
