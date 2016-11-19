18fbda5da639eb871714c778a0e2a0c76f421e12
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-11-19 01:27:03 +0100

    allow for jsx as well

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.js$/,
+      test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: "babel-loader",
       query: {
@@ -31,6 +31,7 @@ module.exports = {
     new ExtractTextPlugin("css/app.css")
   ],
   resolve: {
-    modulesDirectories: ["node_modules", __dirname + "/web/static/js"]
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: ["node_modules", __dirname + "/web/static/js"],
   }
 };
