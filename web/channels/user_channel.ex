fac110234d7c1a9814b26fe78b0e6f1feb769ccb
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2016-12-12 21:21:01 +0100

    add user socket with channel

---
+++ web/channels/user_channel.ex
@@ -0,0 +1,13 @@
+defmodule Eliscore.UserChannel do
+  use Eliscore.Web, :channel
+
+  def join("users:" <> user_id, _params, socket) do
+    current_user = socket.assigns.current_user
+
+    if String.to_integer(user_id) == current_user.id do
+      {:ok, socket}
+    else
+      {:error, %{reason: "Invalid user"}}
+    end
+  end
+end
