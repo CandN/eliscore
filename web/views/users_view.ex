e6990fcecc849cb2c4710274b2befa7f98c8e166
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2017-02-19 22:04:59 +0100

    add json view for action

--- web/views/users_view.ex
+++ web/views/users_view.ex
@@ -5,6 +5,14 @@ defmodule Eliscore.UsersView do
     %{users: users}
   end
 
+  def render("show.json", %{user: user}) do
+    %{user: user}
+  end
+
+  def render("error.json", _) do
+    %{error: "User not found"}
+  end
+
   def template_not_found(_template, assigns) do
     render "500.html", assigns
   end
