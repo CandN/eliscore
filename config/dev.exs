29fa5955043f4dbd7ca0d3faa69802bf5ea359fc
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2017-02-19 18:06:38 +0100

    watch scss too

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :eliscore, Eliscore.Endpoint,
 config :eliscore, Eliscore.Endpoint,
   live_reload: [
     patterns: [
-      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
+      ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg|scss)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
       ~r{web/templates/.*(eex)$}
