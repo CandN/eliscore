131384a5c5d3d3d0f02c7438abb553f83930308f
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2017-03-09 23:03:52 +0100

    modify deploymator to work better with post-receive hook

--- bin/deploymator.sh
+++ bin/deploymator.sh
@@ -4,16 +4,22 @@ export MIX_ENV=prod
 export PORT=8888
 export NODE_ENV=production
 
+APP_ROOT=/home/eliscore
+GIT_DIR=/home/git/projects/eliscore
 RELEASE_VERSION=$(cat mix.exs | grep version | awk '{print $2}' | awk '{gsub(/"/, ""); print}' | sed 's/.$//')
 
-webpack -p --config ./webpack.config.prod.js
-mix compile
-mix phoenix.digest
-mix release
-mix ecto.migrate
-cp ~/eliscore/rel/eliscore/releases/"$RELEASE_VERSION"/eliscore.tar.gz /home/eliscore/
-cd /home/eliscore && tar xzf /home/eliscore/eliscore.tar.gz
+cd $GIT_DIR && yes | mix local.hex
+cd $GIT_DIR && mix deps.get
+cd $GIT_DIR && npm install
+
+cd $GIT_DIR && webpack -p --config ./webpack.config.prod.js
+cd $GIT_DIR && mix compile
+cd $GIT_DIR && mix phoenix.digest
+cd $GIT_DIR && mix release
+cd $GIT_DIR && mix ecto.migrate
+cp $GIT_DIR/rel/eliscore/releases/"$RELEASE_VERSION"/eliscore.tar.gz $APP_ROOT
+cd $APP_ROOT && tar xzf /home/eliscore/eliscore.tar.gz
 
 PID=$(ps aux | grep [b]eam | awk '{print $2}')
 kill -9 "$PID"
-/bin/bash /home/eliscore/bin/eliscore start
+/bin/bash $APP_ROOT/bin/eliscore start
