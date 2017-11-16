db30aa0fe84b10c3736746e9a7b778c9c3c812c4
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2017-11-17 00:25:44 +0100

    add wildcard path

--- lib/eliscore_web/router.ex
+++ lib/eliscore_web/router.ex
@@ -39,6 +39,7 @@ defmodule EliscoreWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
