f4cd83f1b542fc7defcce269e2201f191930407f
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2017-11-17 00:25:13 +0100

    update config for db and guardian

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,13 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :guardian, Guardian,
+  issuer: "Eliscore",
+  ttl: { 3, :days },
+  verify_issuer: true,
+  secret_key: "ajaha",
+  serializer: Eliscore.GuardianSerializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
