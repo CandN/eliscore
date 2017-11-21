8018c526e8fe178a7ee04fc1827736c1fd3ab53d
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2017-11-21 21:48:40 +0100

    update proptypes imports

--- assets/js/components/separator.js
+++ assets/js/components/separator.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const Separator = ({ text }) => (
   <div className="col-xs-2 match__separator">{text}</div>
