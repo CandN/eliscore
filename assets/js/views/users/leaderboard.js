1cb5356fca102abfe4420ed0690c7afc65851ab8
Artur Okoński <okonski.a.dev@gmail.com>
2018-01-22 21:37:13 +0100

    Fetch and display leaderboard

--- assets/js/views/users/leaderboard.js
+++ assets/js/views/users/leaderboard.js
@@ -5,17 +5,32 @@ class Leaderboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      leaderboard: ['Artur', 'Siepet', 'Robcio'],
+      leaderboard: [],
     }
+  }
+  
+  componentDidMount() {
+    axios.get('/api/v1/users')
+      .then(response => {
+        let players = response.data.data;
+        this.setState({ leaderboard: players });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
     return (
-      <div>
-        <h2>Leaderboard</h2>
-        <ul className="leaderboard">
-          { this.state.leaderboard.map(function(player) {
-              return <li className="leaderboard__player">{player}</li>;
+      <div className='leaderboard'>
+        <ul className='leaderboard__players-list'>
+          { this.state.leaderboard.map((player, i) => {
+              return (
+                <li className="leaderboard__player">
+                  <span className='leaderboard__position'>{i + 1}</span>
+                  {player.full_name}
+                </li>
+              )
             })
           }
         </ul>
