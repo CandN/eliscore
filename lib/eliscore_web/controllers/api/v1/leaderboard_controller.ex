be89d325afcebf56fdf604d47a4778e2edf7f5e6
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2018-01-31 09:27:44 +0100

    split multiline

--- lib/eliscore_web/controllers/api/v1/leaderboard_controller.ex
+++ lib/eliscore_web/controllers/api/v1/leaderboard_controller.ex
@@ -5,7 +5,14 @@ defmodule EliscoreWeb.LeaderboardController do
   import Ecto.Query, only: [from: 2]
 
   def index(conn, _params) do
-    q = from gm in GameMatch, select: %{wins: fragment("COUNT(CASE WHEN player1_score > player2_score THEN player1_id ELSE player2_id END) as wins"), winner: fragment("CASE WHEN player1_score > player2_score THEN u1.full_name ELSE u2.full_name END as winner")}, join: u in assoc(gm, :player1), join: u2 in assoc(gm, :player2), group_by: fragment("winner"), order_by: fragment("wins DESC")
+    q = from gm in GameMatch,
+    select: %{
+      wins: fragment("COUNT(CASE WHEN player1_score > player2_score THEN player1_id ELSE player2_id END) as wins"),
+      winner: fragment("CASE WHEN player1_score > player2_score THEN u1.full_name ELSE u2.full_name END as winner")},
+     join: u in assoc(gm, :player1),
+     join: u2 in assoc(gm, :player2),
+     group_by: fragment("winner"),
+     order_by: fragment("wins DESC")
 
     players = Repo.all(q)
 
