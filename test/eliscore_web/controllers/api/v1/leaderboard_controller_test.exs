5b82496e78907b2ecdd67b312d9af9a91be055a6
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2018-02-16 17:21:51 +0100

    fix specs and factoryies

--- test/eliscore_web/controllers/api/v1/leaderboard_controller_test.exs
+++ test/eliscore_web/controllers/api/v1/leaderboard_controller_test.exs
@@ -1,91 +1,59 @@
 defmodule EliscoreWeb.LeaderboardControllerTest do
   use EliscoreWeb.ConnCase, async: true
+  import Eliscore.Factory
   alias Eliscore.{Repo, User, GameMatch}
 
   test "index/2 responds with maps based on game matches won" do
-    users = [
-      User.changeset(
-        %User{},%{
-          full_name: "John Doe",
-          first_name: "John",
-          last_name: "Doe",
-          image_url: "image/url",
-          uuid: "11111111",
-          email: "test1@test.test",
-          password: "password"
-        }),
-      User.changeset(
-        %User{},%{
-          full_name: "Joe Dohn",
-          first_name: "Joe",
-          last_name: "Dohn",
-          image_url: "image/url",
-          uuid: "11111111",
-          email: "test2@test.test",
-          password: "password"
-        }),
-      User.changeset(
-        %User{},%{
-          full_name: "Dohn Joe",
-          first_name: "Dohn",
-          last_name: "Joe",
-          image_url: "image/url",
-          uuid: "11111111",
-          email: "test3@test.test",
-          password: "password"
-        })]
-
-    Enum.each(users, &Repo.insert!(&1))
-    user_1 = Repo.get_by(User, first_name: "John")
-    user_2 = Repo.get_by(User, first_name: "Dohn")
-    user_3 = Repo.get_by(User, first_name: "Joe")
+    user1 = insert(:user, full_name: "John Wayne")
+    user2 = insert(:user, full_name: "John Rambo")
+    user3 = insert(:user, full_name: "John Travolta")
 
     matches = [
       GameMatch.changeset(
         %GameMatch{},%{
-          player1_id: user_1.id,
+          player1_id: user1.id,
           player1_score: 10,
-          player2_id: user_2.id,
+          player2_id: user2.id,
           player2_score: 9
         }
       ),
       GameMatch.changeset(
         %GameMatch{},%{
-          player1_id: user_1.id,
+          player1_id: user1.id,
           player1_score: 10,
-          player2_id: user_2.id,
+          player2_id: user2.id,
           player2_score: 10
         }
       ),
       GameMatch.changeset(
         %GameMatch{},%{
-          player1_id: user_2.id,
+          player1_id: user2.id,
           player1_score: 10,
-          player2_id: user_3.id,
+          player2_id: user3.id,
           player2_score: 10
         }
       ),
       GameMatch.changeset(
         %GameMatch{},%{
-          player1_id: user_2.id,
+          player1_id: user2.id,
           player1_score: 10,
-          player2_id: user_3.id,
+          player2_id: user3.id,
           player2_score: 10
         }
       ),
       GameMatch.changeset(
         %GameMatch{},%{
-          player1_id: user_2.id,
+          player1_id: user2.id,
           player1_score: 10,
-          player2_id: user_3.id,
+          player2_id: user3.id,
           player2_score: 10
         }
       ),
       GameMatch.changeset(
         %GameMatch{},%{
-          player1_id: user_2.id,
+          player1_id: user2.id,
           player1_score: 10,
-          player2_id: user_3.id,
+          player2_id: user3.id,
           player2_score: 10
         }
       )]
@@ -93,13 +61,14 @@ defmodule EliscoreWeb.LeaderboardControllerTest do
     Enum.each(matches, &Repo.insert!(&1))
 
     response = build_conn()
+               |> Eliscore.Guardian.Plug.sign_in(user1)
                |> get(leaderboard_path(build_conn(), :index))
                |> json_response(200)
 
     expected = %{
       "data" => [
         %{
-          "full_name" => "Dohn Joe",
+          "full_name" => "John Rambo",
           "wins" => 0,
           "all_games" => 6,
           "loses" => 1,
@@ -107,7 +76,7 @@ defmodule EliscoreWeb.LeaderboardControllerTest do
           "points" => 5
         },
         %{
-          "full_name" => "John Doe",
+          "full_name" => "John Wayne",
           "wins" => 1,
           "all_games" => 2,
           "loses" => 0,
@@ -115,7 +84,7 @@ defmodule EliscoreWeb.LeaderboardControllerTest do
           "points" => 4
         },
         %{
-          "full_name" => "Joe Dohn",
+          "full_name" => "John Travolta",
           "wins" => 0,
           "all_games" => 4,
           "loses" => 0,
--- test/eliscore_web/controllers/category_controller_test.exs
+++ test/eliscore_web/controllers/category_controller_test.exs
@@ -1,5 +1,6 @@
 defmodule EliscoreWeb.CategoryControllerTest do
   use EliscoreWeb.ConnCase
+  import Eliscore.Factory
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -7,6 +8,8 @@ defmodule EliscoreWeb.CategoryControllerTest do
 
   describe "index" do
     test "lists all categories", %{conn: conn} do
+      user = insert(:user)
+      conn = conn |> Eliscore.Guardian.Plug.sign_in(user)
       conn = get conn, category_path(conn, :index)
       assert json_response(conn, 200)["data"] == []
     end
