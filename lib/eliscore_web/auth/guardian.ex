a5270de263c51d7253c9db8ee9b06ffe0b300b36
Marcin Siepetowski <marcin.siepetowski@netguru.pl>
2018-02-16 11:33:18 +0100

    add hooks

--- lib/eliscore_web/auth/guardian.ex
+++ lib/eliscore_web/auth/guardian.ex
@@ -17,4 +17,28 @@ defmodule Eliscore.Guardian do
   def resource_from_claims(_claims) do
     {:error, :reason_for_error}
   end
+
+  def after_encode_and_sign(resource, claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.after_encode_and_sign(resource, claims["typ"], claims, token) do
+      {:ok, token}
+    end
+  end
+
+  def on_verify(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_verify(claims, token) do
+      {:ok, claims}
+    end
+  end
+
+  def on_refresh({old_token, old_claims}, {new_token, new_claims}, _options) do
+    with {:ok, _} <- Guardian.DB.on_refresh({old_token, old_claims}, {new_token, new_claims}) do
+      {:ok, {old_token, old_claims}, {new_token, new_claims}}
+    end
+  end
+
+  def on_revoke(claims, token, _options) do
+    with {:ok, _} <- Guardian.DB.on_revoke(claims, token) do
+      {:ok, claims}
+    end
+  end
 end
