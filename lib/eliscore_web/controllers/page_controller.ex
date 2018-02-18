1b0ebb5c6622af2e3065e80b5c04c7e6a6d1f3f9
Kamil Zieliński <kamil.zielinski@netguru.co>
2018-02-18 23:34:18 +0100

    add logic

--- lib/eliscore_web/controllers/page_controller.ex
+++ lib/eliscore_web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule EliscoreWeb.PageController do
   use EliscoreWeb, :controller
 
+  alias Eliscore.{Repo, Model.TournamentDate}
+  alias Ecto.Query
+
   def index(conn, _params) do
-    render conn, "index.html"
+    date = TournamentDate |> Query.first(:date) |> Repo.one
+    tournament_date = date.date |> DateTime.to_string
+    render conn, "index.html", date: tournament_date
   end
 end
