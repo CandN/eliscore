d243d4edb35c00fdef073d35ec0acbbce6dd4279
Kamil Zielinski <Kamil.Zielinski.extern@solarisbank.de>
2018-06-06 17:26:51 +0200

    fix registrations controller

---
+++ lib/eliscore_web/controllers/api/v1/registration_controller.ex
@@ -0,0 +1,11 @@
+defmodule EliscoreWeb.RegistrationController do
+  use EliscoreWeb, :controller
+
+  alias Eliscore.Auth
+
+  plug :scrub_params, "user" when action in [:create]
+
+  def create(conn, %{"user" => user_params}) do
+    Auth.sign_in_user(conn, user_params)
+  end
+end
